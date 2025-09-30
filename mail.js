38fbd36959f4b7462c8cfadf38395d041480003f
Baldwin <107270233+themirze@users.noreply.github.com>
2025-09-30 11:53:43 +0200

    Update mail.js

--- mail.js
+++ mail.js
@@ -1,46 +1,60 @@
 function() {
-  var formInputs = {{DLV - inputs}};
+  var formId = {{dLv - formId}} || ''; 
+  var formInputs = {{dLv - inputs}};
 
   if (!formInputs) {
     return undefined;
   }
 
+  var whatsappFormIds = ['6930', '7208', '8309'];
+  var emailOnlyFormIds = ['7214', '4793', '1066', '7211', '4284', '770'];
+
+  var searchMode = 'default';
+  if (whatsappFormIds.indexOf(formId.toString()) > -1) {
+    searchMode = 'phone_only'; // Sadece telefon ara
+  } else if (emailOnlyFormIds.indexOf(formId.toString()) > -1) {
+    searchMode = 'email_only'; // Sadece email ara
+  }
+
   var foundEmail = null;
   var foundPhone = null;
+
   for (var key in formInputs) {
     if (formInputs.hasOwnProperty(key)) {
       var value = formInputs[key];
 
-      // check there is mail or not
-      if (typeof value === 'string' && value.includes('@')) {
+      if (searchMode !== 'phone_only' && typeof value === 'string' && value.includes('@')) {
         foundEmail = value.toLowerCase().trim();
+        break;
       }
-      
-      // check there is phone or not
-      if (typeof value === 'string' && /^[0-9\s\-\+\(\)]+$/.test(value) && value.length >= 10) {
-        var rawPhone = value.replace(/[^0-9]/g, '');
-        // E.164 formatted
-        if (rawPhone.length >= 10 && !rawPhone.startsWith('90')) {
-           foundPhone = '+90' + rawPhone.slice(-10);
-        } else if (rawPhone.startsWith('90')) {
-           foundPhone = '+' + rawPhone;
+    
+      if (searchMode !== 'email_only' && !foundEmail && typeof value === 'string' && /^\+?[0-9\s\-\(\)]{10,}$/.test(value)) {
+        
+        var cleanedPhone = value.replace(/[\s\-\(\)]/g, '');
+
+        if (cleanedPhone.startsWith('+')) {
+            foundPhone = cleanedPhone;
+        } else {
+            cleanedPhone = cleanedPhone.replace(/[^0-9]/g, '');
+            foundPhone = '+' + cleanedPhone;
+        }
+
+        if (searchMode === 'phone_only') {
+          break;
         }
       }
     }
   }
-  
+
   if (foundEmail) {
     return {
       'email': foundEmail
     };
-  }
-
-  else if (foundPhone) {
+  } else if (foundPhone) {
     return {
       'phone_number': foundPhone
     };
   }
 
-  // Hiçbir şey bulunamazsa tanımsız döndür.
   return undefined;
 }
